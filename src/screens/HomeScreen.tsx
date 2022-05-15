9bf272015e3a34bb0a77c939098539b049203e14
Bikcode <bikcodeh@gmail.com>
2022-05-15 18:10:18 -0500

    create partial implementation for hook paginated and create pokemonApi

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Image, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from './../theme/AppTheme';
+import { usePokemonPaginated } from './../hooks/UsePokemonPaginated';
 
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
+    usePokemonPaginated();
     return (
         <>
             <Image
