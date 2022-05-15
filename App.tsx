cc7a71b0b7f1383a9a11a4fc688366a668b62153
Bikcode <bikcodeh@gmail.com>
2022-05-15 16:56:13 -0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+    return (
+      <>
+        <Text>App Screen</Text>
+      </>
+    )
+}
+
+export default App;
